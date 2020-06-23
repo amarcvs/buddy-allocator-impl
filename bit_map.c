4192214d8c57209c7aa3c404b1e5702408215610
Marco Alberti <alberti.1794436@studenti.uniroma1.it>
2020-06-23 09:55:45 +0200

    fixing bitmap functions

--- bit_map.c
+++ bit_map.c
@@ -3,7 +3,7 @@
 
 // returns the number of bytes to store bits booleans
 int BitMap_getBytes(int bits){
-  return bits/8 + (bits%8)!=0;
+  return bits/8 + ((bits%8)!=0);
 }
 
 // initializes a bitmap on an external array
@@ -17,13 +17,15 @@ void BitMap_init(BitMap* bit_map, int num_bits, uint8_t* buffer){
   bit_map->buffer_size=BitMap_getBytes(num_bits);
 }
 
-// sets a the bit bit_num in the bitmap
+// sets the bit bit_num in the bitmap
 // status= 0 or 1
 void BitMap_setBit(BitMap* bit_map, int bit_num, int status){
   // get byte
   int byte_num=bit_num>>3;
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
+  
+  int bit_in_byte=bit_num%8; // offset bit in byte    
+
   if (status) {
     bit_map->buffer[byte_num] |= (1<<bit_in_byte);
   } else {
@@ -35,6 +37,6 @@ void BitMap_setBit(BitMap* bit_map, int bit_num, int status){
 int BitMap_bit(const BitMap* bit_map, int bit_num){
   int byte_num=bit_num>>3; 
   assert(byte_num<bit_map->buffer_size);
-  int bit_in_byte=byte_num&0x03;
+  int bit_in_byte=bit_num%8;
   return (bit_map->buffer[byte_num] & (1<<bit_in_byte))!=0;
 }
--- bit_map.h
+++ bit_map.h
@@ -1,9 +1,9 @@
 #pragma once
 #include <stdint.h>
 
-// simple bit array
+
 typedef struct  {
-  uint8_t *buffer;  // externally allocated buffer
+  uint8_t *buffer;
   int buffer_size;
   int num_bits; 
 } BitMap;
@@ -14,7 +14,7 @@ int BitMap_getBytes(int bits);
 // initializes a bitmap on an external array
 void BitMap_init(BitMap* bit_map, int num_bits, uint8_t* buffer);
 
-// sets a the bit bit_num in the bitmap
+// sets the bit bit_num in the bitmap
 // status= 0 or 1
 void BitMap_setBit(BitMap* bit_map, int bit_num, int status);
 
